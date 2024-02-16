76ee0d78478edd144349619c1815bad43d8edbf3
James Hawk <jph94880@gmail.com>
2024-02-16 11:56:53 -0600

    Adds css improvements

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,11 +4,11 @@ export default ({ name, real_name, powers, first_appearence, image }) => {
 
   return (
     <section className="card">
+      <img className="card-image" src={image} alt={name}></img>
       <h1 className="name">{name}</h1>
       <p className="real-name">Secret Identity: {real_name}</p>
       <p className="first-appearence">First Appearence: {first_appearence}</p>
       <p className="powers">Powers: {powers}</p>
-      <img className="image" src={image} alt={name}></img>
     </section>
   )
 }
