0a1459e249471b8ed005e5b7ef2cba004d978454
James Hawk <jph94880@gmail.com>
2024-02-17 16:23:33 -0600

    Adds edit button to Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -10,7 +10,8 @@ export default ({ name, real_name, powers, first_appearence, image, avenger_id, 
       <p className="real-name">Secret Identity: {real_name}</p>
       <p className="first-appearence">First Appearence: {first_appearence}</p>
       <p className="powers">Powers: {powers}</p>
-      <button className="deleteButton" onClick={() => handleDelete(avenger_id)}>Delete Avenger</button>
+      <button name="delete" className="deleteButton" onClick={() => handleDelete(avenger_id)}>Delete Avenger</button>
+      <button name="edit" className="editButton" onClick={() => console.log("Edit Avenger!")}>Edit</button>
     </section>
   )
 }
