382245d6f1a8a99abd12686c0ca30f477473006f
James Hawk <jph94880@gmail.com>
2024-02-17 16:59:19 -0600

    Toggles between Avenger Card and form

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,useState } from "react";
+import React, { useContext, useState } from "react";
 
 export default ({
   name,
@@ -11,35 +11,106 @@ export default ({
   handleEditAvenger,
 }) => {
   const [editToggle, setEditToggle] = useState(false);
+    const [value, setValue] = useState({
+      name: name,
+      realName: real_name,
+      firstAppearence: first_appearence,
+      image: image,
+      powers: powers,
+    });
 
   const handleEdit = () => {
-    console.log("handleEdit()")
-    setEditToggle(true)
-  }
+    console.log("handleEdit()");
+    setEditToggle(true);
+  };
+  
+    const handleChange = (e) => {
+      const ourInput = e.target.name
+      console.log(e.target.value)
+      setValue({ ...value, [ourInput]: e.target.value});
+    };
+  
+    const handleReset = () => setValue({
+      name: "",
+      realName: "",
+      firstAppearence: "",
+      image: "",
+      powers: ""
+    })
 
   return (
-    
     <section className="card">
-      {editToggle ? <></> : <> 
-      <img className="card-image" src={image} alt={name}></img>
-      <h1 className="name">{name}</h1>
-      <p className="real-name">Secret Identity: {real_name}</p>
-      <p className="first-appearence">First Appearence: {first_appearence}</p>
-      <p className="powers">Powers: {powers}</p>
-      <button
-        name="delete"
-        className="deleteButton"
-        onClick={() => handleDelete(avenger_id)}
-      >
-        Delete Avenger
-      </button>
-      <button
-        name="edit"
-        className="editButton"
-        onClick={() => handleEdit()}
-      >
-        Edit
-      </button>
-      </>}
+      {editToggle ? (
+        <>
+          <form
+            action=""
+            className="avengers-form"
+            onSubmit={(e) => handleSubmit(e, handleReset)}
+          >
+            <input
+              name="name"
+              placeholder="Avenger Name"
+              type="text"
+              label="Avenger Name"
+              onChange={handleChange}
+              value={value.name}
+            ></input>
+            <input
+              name="realName"
+              placeholder="Real Name"
+              type="password"
+              onChange={handleChange}
+              value={value.realName}
+            ></input>
+            <input
+              name="firstAppearence"
+              placeholder="First Appearence"
+              type="text"
+              onChange={handleChange}
+              value={value.firstAppearence}
+            ></input>
+            <input
+              name="powers"
+              placeholder="Powers"
+              type="text"
+              onChange={handleChange}
+              value={value.powers}
+            ></input>
+            <input
+              name="image"
+              placeholder="Image"
+              type="text"
+              onChange={handleChange}
+              value={value.image}
+            ></input>
+            <button className="button">Create New Avenger</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <img className="card-image" src={image} alt={name}></img>
+          <h1 className="name">{name}</h1>
+          <p className="real-name">Secret Identity: {real_name}</p>
+          <p className="first-appearence">
+            First Appearence: {first_appearence}
+          </p>
+          <p className="powers">Powers: {powers}</p>
+          <button
+            name="delete"
+            className="deleteButton"
+            onClick={() => handleDelete(avenger_id)}
+          >
+            Delete Avenger
+          </button>
+          <button
+            name="edit"
+            className="editButton"
+            onClick={() => handleEdit()}
+          >
+            Edit
+          </button>
+        </>
+      )}
     </section>
-)};
+  );
+};
